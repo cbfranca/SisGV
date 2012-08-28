be4af25c8506d8aff4895bcb14cecda59a13ca21
cbfranca <cleidsonbf@gmail.com>
2012-08-28 01:06:40 -0300

    Criando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SisGV::Application.config.secret_token = 'ea1273949eb6e30bcbcc6eb0a0f96f395765c6002bfb672d3d7b2db38279f2fbfa34e289b5f7a580691f442a090d2e6d1eea9051786b69033d2f85a5a5808047'
