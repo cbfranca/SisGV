e4a972b829b306ab2a93d21817d3852f2e9c78be
cbfranca <cleidsonbf@gmail.com>
2012-11-22 01:37:13 -0300

    alteração em etiquetas

--- app/reports/etiquetas_vocacionados.rb
+++ app/reports/etiquetas_vocacionados.rb
@@ -15,7 +15,7 @@ class EtiquetasVocacionados < Prawn::Document
   Prawn::Labels.types = 'config/pimaco_labels.yml' 
 
     Prawn::Labels.render(@vocacionados, :type => "pimaco_6081") do |pdf, voc|
-      pdf.text voc.nome, :size => 10
+      pdf.text voc.nome[0..50], :size => 10
       pdf.text voc.endereco, :size => 10
       pdf.text voc.bairro + "   " + voc.cidade_id + "   " + Estado.find(voc.cod_estado).nome + "   " +  voc.cep  , :size => 10
     end    
